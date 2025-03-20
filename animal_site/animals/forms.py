138e889336b9ac7d41e56fd6d98c50ed55b7e3fc
jijakchok <dogmajojo@gmail.com>
2025-03-20 17:02:37 +0300

    origin returns

--- animal_site/animals/forms.py
+++ animal_site/animals/forms.py
@@ -4,12 +4,12 @@ from .models import Animal
 class AnimalForm(forms.ModelForm):
     class Meta:
         model = Animal
-        fields = ['comment', 'number', 'location', 'image']
+        fields = ['comment', 'number', 'location']
 
     def clean_number(self):
         number = self.cleaned_data['number']
-        if not number.isdigit() or len(number) != 10:
-            raise forms.ValidationError("Номер должен состоять из 10 цифр.")
+        if not number.isdigit() or len(number) != 11:
+            raise forms.ValidationError("Номер должен состоять из 11 цифр.")
         return number
 
     def clean_location(self):
